8e045686be406f0b9b252559b064dffb3c4690c7
lmarcho <lakshitha.mathngadeera@neosolax.com>
2023-07-13 11:58:53 +0530

    Login ChatRoom

---
+++ src/main/java/com/example/livechatgroup/controller/LoginControllerForm.java
@@ -0,0 +1,72 @@
+package com.example.livechatgroup.controller;
+
+import com.example.livechatgroup.client.Client;
+import com.example.livechatgroup.model.UserModel;
+import com.example.livechatgroup.server.Server;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+
+public class LoginControllerForm implements Initializable {
+
+    public AnchorPane login;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private Button btnJoin;
+
+    @FXML
+    void btnJoinOnAction(ActionEvent event) throws IOException {
+        try {
+            String userName=txtUserName.getText();
+            if(UserModel.userVerified(userName)) {
+                if (Pattern.matches("[a-zA-Z\\s]+", txtUserName.getText())) {
+                    Client client = new Client(txtUserName.getText());
+                    Thread thread = new Thread(client);
+                    thread.start();
+                    Stage stage = (Stage) login.getScene().getWindow();
+                    stage.close();
+                }
+            } else if (UserModel.save(userName)) {
+                if (Pattern.matches("[a-zA-Z\\s]+", txtUserName.getText())) {
+                    Client client = new Client(txtUserName.getText());
+                    Thread thread = new Thread(client);
+                    thread.start();
+                    Stage stage = (Stage) login.getScene().getWindow();
+                    stage.close();
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void txtUserNameOnAction(ActionEvent event) {
+        btnJoin.fire();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            Server server = Server.getServerSocket();
+            Thread thread = new Thread(server);
+            thread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/livechatgroup/model/UserModel.java
+++ src/main/java/com/example/livechatgroup/model/UserModel.java
@@ -1,20 +1,17 @@
 package com.example.livechatgroup.model;
 
-import com.example.livechatgroup.dto.User;
 import com.example.livechatgroup.util.CrudUtil;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserModel {
-    public static User userVerified(String userName) throws SQLException {
-        ResultSet resultSet = CrudUtil.execute("SELECT * FROM user WHERE userName = ?", userName);
-        if (resultSet.next()) {
-            return new User(resultSet.getString(1));
-        }
-        return null;
+    public static boolean userVerified(String userName) throws SQLException {
+        ResultSet resultSet =  CrudUtil.execute("SELECT * FROM user WHERE userName = ?",userName);
+        return resultSet.next() ? true : false;
     }
 
-    public boolean add(User dto) throws SQLException {
-        return CrudUtil.execute("INSERT INTO user(userName) VALUES (?)");
+    public static boolean save(String userName) throws SQLException {
+        String sql = "INSERT INTO user(userName) VALUES(?)";
+        return CrudUtil.execute(sql, userName);
     }
 }
